5c4e6e28baa16478b647c3a19211a7107a1de1f5
LiaVernaet <l_vernaet@fanshaweonline.ca>
2019-03-21 13:22:00 -0400

    updated js

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Coca Cola' });
-  res.render('index', { flavorsTitle: 'Our Flavors' });
+  res.render('index', { title: 'Coca Cola', flavorsTitle: 'Our Flavors' });
 });
 
 module.exports = router;
