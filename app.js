3f311d8cd62c0c72625976b306711504ef07f45a
LiaVernaet <l_vernaet@fanshaweonline.ca>
2019-05-29 14:57:25 -0400

    test

--- app.js
+++ app.js
@@ -49,12 +49,15 @@ app.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
 
+
+
+
 var textArray = [
     'song1.ogg',
     'song2.ogg'
 ];
 
-// var randomNumber = Math.floor(Math.random()*textArray.length);
+ var randomNumber = Math.floor(Math.random()*textArray.length);
 
 // catName.setAttribute('src', textArray[randomNumber]);
 
